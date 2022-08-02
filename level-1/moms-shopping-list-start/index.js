e9b2bfc51dbbfc47ed5d56d7f249a77bde5f73d4
Jasean Figgins <figginsjaseane@gmail.com>
2022-08-02 09:04:36 -0400

    Added List Items and edit Buttons

---
+++ level-1/moms-shopping-list-start/index.js
@@ -0,0 +1,55 @@
+const list = document.getElementById('list');
+const form = document.addItem;
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const listItems = form.title.value;
+    const item = document.createElement('div');
+    const li = document.createElement('li');
+
+    form.title.value = "";
+
+    item.style.fontSize = 'large';
+    item.textContent = listItems;
+    list.appendChild(li);
+    li.appendChild(item);
+   
+
+    const edit = document.createElement("button");
+
+    edit.textContent = "Edit Items";
+    li.appendChild(edit);
+    
+    const clear = document.createElement("button");
+
+    clear.textContent = "Item Found";
+    li.appendChild(clear);
+
+    clear.addEventListener ("click",()=> {
+        list.removeChild(li);
+    })
+
+    edit.addEventListener("click",() =>{
+        event.preventDefault();
+        const editInput = document.createElement ("input");
+        const save = document.createElement("button");
+
+        save.textContent = "Save Item";
+        save.type = "submit";
+        editInput.placeholder = item.textContent;
+        li.appendChild(editInput);
+        li.removeChild(edit);
+        li.appendChild(save);
+
+        save.addEventListener("click" ,()=>{
+            item.textContent = editInput.value
+        })
+
+       
+        
+
+
+    })
+
+
+})
